0e36f184252c0c78190171d95019227b95b91c2a
Rohit Sankaran <rohit.sankaran@gmail.com>
2013-03-22 19:49:40 +0100

    Moved generic utilities to utils, cleaned up Resource

--- s3fileup/api/resources.py
+++ s3fileup/api/resources.py
@@ -1,14 +1,11 @@
 import os
 
-from boto.s3.connection import S3Connection, OrdinaryCallingFormat
-from boto.s3.cors import CORSConfiguration
-from boto.exception import S3CreateError
-from django.conf import settings
-
 from tastypie import fields, utils
 from tastypie.resources import Resource
 from tastypie.authentication import BasicAuthentication
 from tastypie.authorization import Authorization
+
+from s3fileup.utils import generate_signed_url
 
 
 class S3PutResource(Resource):
@@ -23,49 +20,6 @@ class S3PutResource(Resource):
         resource_name = 's3put'
 
 
-    def _generate_bucket_name(self):
-        """
-        TODO: Generate custom bucket name
-        """
-        AWS_STORAGE_BUCKET_NAME = 'app_mi_assets'
-        return AWS_STORAGE_BUCKET_NAME
-
-
-    def _generate_signed_url(self, obj_name, obj_type, method='GET'):
-        """
-        This method generates a temporary URL for an object in S3
-        """
-        AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-        AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-
-        conn = S3Connection(
-            aws_access_key_id=AWS_ACCESS_KEY_ID,
-            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-            calling_format=OrdinaryCallingFormat()
-        )
-
-        bucket_name = self._generate_bucket_name()
-        try:
-            bucket = conn.create_bucket(bucket_name)
-        except S3CreateError:
-            bucket =conn.lookup(bucket_name)
-
-        if method in ('PUT', 'POST'):
-            # Setting CORS config (Cross Origin Resource)
-            cors_cfg = CORSConfiguration()
-            cors_cfg.add_rule(['PUT', 'POST', 'DELETE'], 'http://localhost:5000', allowed_header='*', max_age_seconds=3000, expose_header='x-amz-server-side-encryption')
-            cors_cfg.add_rule('GET', '*')
-            bucket.set_cors(cors_cfg)
-
-        headers= {
-            'Content-Type': obj_type,
-            'x-amz-acl' : 'public-read',
-        }
-
-        url = conn.generate_url(300, method, bucket=bucket_name, key=obj_name, headers=headers)
-
-        return url
-
     def post_list(self, request, **kwargs):
         s3_object_type = request.POST.get('s3_object_type', 'application/octet-stream')
         s3_object_name = request.POST.get('s3_object_name', None)
@@ -73,7 +27,7 @@ class S3PutResource(Resource):
         if None in (s3_object_type, s3_object_name):
             raise HttpBadRequest()
 
-        url = self._generate_signed_url(s3_object_name, s3_object_type, method='PUT')
+        url = generate_signed_url(s3_object_name, s3_object_type, method='PUT')
 
         result = { 'url' : url }
         return self.create_response(request, result)
